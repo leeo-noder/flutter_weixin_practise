76e9c137f2b6cfccfb72faf58b36f6a5390b5bef
457937015 <457937015@qq.com>
2019-02-27 16:45:07 +0800

    一些基础项配置

---
+++ lib/components/BottomNavigationBar/bar.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weixin/common/style/GSYStyle.dart';
+import 'package:flutter_weixin/model/widget.dart';
+import 'package:flutter_weixin/routers/application.dart';
+
+class Bar extends StatefulWidget {
+  Bar({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _BarState();
+  }
+}
+
+class _BarState extends State<Bar> with SingleTickerProviderStateMixin {
+  WidgetControlModel widgetControlModel = new WidgetControlModel();
+  TabController tabController;
+  String appBarTitle = tabData[0]['text'];
+  static List tabData = [
+    {'text': '微信', 'icon': new Icon(GSYICons.HOME_CHECKED)},
+    {'text': '通讯录', 'icon': new Icon(GSYICons.ADDRESS_BOOK)},
+    {'text': '发现', 'icon': new Icon(GSYICons.FOUND)},
+    {'text': '我', 'icon': new Icon(GSYICons.WO)}
+  ];
+  List<Widget> myTabs = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('xxxx');
+    tabController = new TabController(length: 4, vsync: this, initialIndex: 0);
+    for (int i = 0; i < tabData.length; i++) {
+      myTabs.add(new Tab(text: tabData[i]['text'], icon: tabData[i]['icon']));
+    }
+    tabController.addListener(() {
+      if (tabController.indexIsChanging) {
+        _onTabChange();
+      }
+    });
+    Application.controller = tabController;
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  _onTabChange() {
+    if (this.mounted) {
+      this.setState(() {
+        appBarTitle = tabData[tabController.index]['text'];
+      });
+    }
+  }
+
+  _buildPopupMenuItem(IconData icon, String title) {
+    return Row(children: <Widget>[
+      Icon(
+        icon,
+        color: Color(0xFFFFFFFF),
+      ),
+      Container(width: 12.0),
+      Text(
+        title,
+        style: TextStyle(color: Color(0xFFFFFFFF)),
+      )
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(appBarTitle),
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            Container(width: 14.0),
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuItem<String>>[
+                  PopupMenuItem(
+                    child: _buildPopupMenuItem(GSYICons.HOME_CHECKED, '发起群聊'),
+                    value: "1",
+                  ),
+                  PopupMenuItem(
+                    child: _buildPopupMenuItem(
+                        GSYICons.ADDRESS_BOOK_CHECKED, '添加朋友'),
+                    value: "2",
+                  )
+                ];
+              },
+              icon: Icon(Icons.add_circle_outline),
+              onSelected: (String selected) {},
+            ),
+          ],
+        ),
+        body: Container(
+          child: ListView(
+            children: <Widget>[],
+          ),
+          color: Color(GSYColors.primaryLightValue),
+        ),
+        bottomNavigationBar: Material(
+          color: Color(GSYColors.primaryValue),
+          child: SafeArea(
+              child: Container(
+            child: TabBar(
+                controller: tabController,
+                labelColor: Colors.green,
+                unselectedLabelColor: Colors.black,
+                tabs: myTabs),
+          )),
+        ));
+  }
+}
---
+++ lib/components/BottomNavigationBar/demo.dart
@@ -0,0 +1,56 @@
+/**
+ * Created with Android Studio.
+ * User: ryan
+ * Date: 2018/12/27
+ * Time: 下午6:27
+ * email: zhu.yan@alibaba-inc.com
+ * tartget: BottomNavigationBar 的示例
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter_weixin/common/style/GSYStyle.dart';
+/*
+* BottomNavigationBar 默认的实例
+* */
+class BottomNavigationBarFullDefault extends StatefulWidget {
+  const BottomNavigationBarFullDefault() : super();
+  @override
+  State<StatefulWidget> createState() => _BottomNavigationBarFullDefault();
+}
+
+/*
+* BottomNavigationBar 默认的实例,有状态
+* */
+class _BottomNavigationBarFullDefault extends State {
+   int _currentIndex = 1;
+
+  void _onItemTapped(int index) {
+    if(mounted) {
+      setState(() {
+        _currentIndex = index;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed, // BottomNavigationBarType 中定义的类型，有 fixed 和 shifting 两种类型
+      iconSize: 24.0, // BottomNavigationBarItem 中 icon 的大小
+      currentIndex: _currentIndex, // 当前所高亮的按钮index
+      onTap: _onItemTapped, // 点击里面的按钮的回调函数，参数为当前点击的按钮 index
+      fixedColor: Colors.green, // 如果 type 类型为 fixed，则通过 fixedColor 设置选中 item 的颜色
+      items: <BottomNavigationBarItem> [
+        BottomNavigationBarItem(
+            title:  Text("微信"), icon:  Icon(GSYICons.HOME), activeIcon: Icon(GSYICons.HOME_CHECKED)),
+        BottomNavigationBarItem(
+            title:  Text("通讯录"), icon:  Icon(GSYICons.ADDRESS_BOOK), activeIcon: Icon(GSYICons.ADDRESS_BOOK_CHECKED)),
+        BottomNavigationBarItem(
+            title:  Text("发现"), icon:  Icon(GSYICons.FOUND), activeIcon: Icon(GSYICons.FOUND_CHECKED)),
+        BottomNavigationBarItem(
+            title:  Text("我"), icon:  Icon(GSYICons.WO), activeIcon: Icon(GSYICons.WO_CHECKED)),
+      ],
+    );
+  }
+}
+
