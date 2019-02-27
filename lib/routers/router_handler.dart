184c93ac30e35e5c14c2fcd491805423442c5e02
457937015 <457937015@qq.com>
2019-02-27 16:36:27 +0800

    一些基础项配置，tab设置

---
+++ lib/routers/router_handler.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter_weixin/main.dart';
+
+// app的首页
+var homeHandler = new Handler(
+  handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+    return new MyHomePage();
+  },
+);
+
+var categoryHandler = new Handler(
+  handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+    String name = params["type"]?.first;
+
+    return null;
+  },
+);
+
+var widgetNotFoundHandler = new Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  return null;
+});
+
+var fullScreenCodeDialog = new Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  String path = params['filePath']?.first;
+  return null;
+});
+
+var webViewPageHand = new Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  String title = params['title']?.first;
+  String url = params['url']?.first;
+  return null;
+});
