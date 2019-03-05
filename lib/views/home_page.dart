9802501bb5bf0e7a280d5db7221652801f746e3d
457937015 <457937015@qq.com>
2019-03-05 16:56:46 +0800

    聊天页面布局设置(上拉下拉刷新)

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,29 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:flutter_weixin/model/conversation.dart'
     show Conversation, mockConversation;
 import 'package:flutter_weixin/utils/net_utils.dart';
-
+import 'package:flutter_weixin/components/PullLoadWidget.dart';
+import 'package:flutter_weixin/components/ListState.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin {
+class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin<HomePage>, ListState<HomePage>, WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) {
-        return _ConversationItem(conversation: mockConversation[index]);
-      },
-      itemCount: mockConversation.length,
-    );
+        return PullLoadWidget(
+          pullLoadWidgetControl,
+              (BuildContext context, int index) => _ConversationItem(conversation: pullLoadWidgetControl.dataList[index]),
+          handleRefresh,
+          onLoadMore,
+          refreshKey: refreshIndicatorKey,
+        );
   }
   @override
+  bool get isRefreshFirst => false;
+  @override
   bool get wantKeepAlive => true;
+  @override
+  Future<Null> handleRefresh() async {
+    if (isLoading) {
+      return null;
+    }
+    isLoading = true;
+    page = 1;
+    mockConversation.clear();
+    await getIndexListData(page);
+    setState(() {
+      pullLoadWidgetControl.needLoadMore = (mockConversation != null && mockConversation.length == 10);
+    });
+    isLoading = false;
+    return null;
+  }
+  // 紧跟在initState之后调用
+  @override
+  void didChangeDependencies() {
+    pullLoadWidgetControl.dataList = mockConversation;
+    if (pullLoadWidgetControl.dataList.length == 0) {
+      showRefreshLoading();
+    }
+    super.didChangeDependencies();
+  }
+  @override
+  Future<Null> onLoadMore() async {
+    if (isLoading) {
+      return null;
+    }
+    isLoading = true;
+    page++;
+    await getIndexListData(page);
+    setState(() {
+      pullLoadWidgetControl.needLoadMore = (mockConversation != null && mockConversation.length < 20);
+    });
+    isLoading = false;
+    return null;
+  }
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getIndexListData();
+   // getIndexListData(1);
   }
 
   @override
@@ -33,9 +75,9 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
     super.setState(fn);
   }
 
-  getIndexListData([Map<String, dynamic> params]) async {
+  getIndexListData(page) async {
     try {
-      var response = await NetUtils.get('https://randomuser.me/api/?results=15');
+      var response = await NetUtils.get('https://randomuser.me/api/?results=10');
       setState(() {
         for (int i = 0; i < response['results'].length; i++) {
           mockConversation.add(Conversation.fromJson(response['results'][i]));
